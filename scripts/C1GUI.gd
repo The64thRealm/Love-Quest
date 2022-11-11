2f5ecd35f3688eb94724016f22aae66251e25ada
Natasha <wong.natasha.m@gmail.com>
2022-11-10 20:42:41 -0800

    city2 and apartment map

---
+++ scripts/C1GUI.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+var action_state = "Off"
+
+func _process(delta):
+	if Input.is_action_just_pressed("Action"):
+		match action_state:
+			"Off":
+				pass
+			"Open":
+				get_tree().change_scene("res://scenes/city 2.tscn")
+				pass
+
+func OnSubwayAreaEntered(body):
+	action_state = "Open"
+	print("inDoor")
+	
+func OnSubwayAreaExited(body):
+	action_state = "Off"
+	print("outDoor")
---
+++ scripts/GUI2.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+var action_state = "Off"
+
+func _process(delta):
+	if Input.is_action_just_pressed("Action"):
+		match action_state:
+			"Off":
+				pass
+			"Open":
+				get_tree().change_scene("res://scenes/apartment.tscn")
+				pass
+
+func OnApartAreaEntered(body):
+	action_state = "Open"
+	print("inDoor")
+	
+func OnApartAreaExited(body):
+	action_state = "Off"
+	print("outDoor")
