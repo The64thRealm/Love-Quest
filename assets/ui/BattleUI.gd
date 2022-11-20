a7019e1a149ace458a45fbd24d154d9a7b20bb10
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2022-11-20 01:05:33 -0800

    Modified DialogBox to work with picking choices
    BATTLEUI DOES NOT WORK IN THIS COMMIT LOL
    (because I'm tired and I'll work on it tomorrow)
    
    Dialog box does basically everything that BattleUI did except for dealing with the patience bar
    the idea is that BattleUI will now extend DialogBox
    
    Also I uh renamed dialogbox because the fact that it was called dialogue instead of dialog bothered me
    
    Changed battle button to dialog button, later on will make a battle button that extends dialog button
    
    edited the dialog file to include a branch in the choice of harper's dialog

--- assets/ui/BattleUI.gd
+++ assets/ui/BattleUI.gd
@@ -1,95 +1,4 @@
-extends Node2D
-const DialogueBox = preload("DialogueBox.gd")
-
-const defaultFilePath = "res://assets/characters/"
-
-export(String) var character
-export(String, FILE, "*.json") var battleFile = "res://assets/ui/testBattle.json"
-var lines = []
-var currentLine = 0 
-var buttons = ["Action1", "Action2"]
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	play()
-	currentLine = -1
-	advanceLine()
-
-func play():
-	show()
-	lines = loadBattle()
-
-func advanceLine():
-	currentLine += 1
-	
-	if currentLine >= len(lines):
-		battleWon() 
-		return
-	
-	updateUI()
-
-func skipToLine(line):
-	currentLine = line
-	
-	if currentLine >= len(lines):
-		battleWon()
-		return
-	
-	updateUI()
-
-func updateUI():
-	updateDialog()
-	updatePatienceDecay()
-	updateActions()
-	updateSprite()
-
-func determineIfClearLine():
-	return 'clear' in lines[currentLine] and lines[currentLine]['clear']
-
-func updateDialog():
-	if 'text' in lines[currentLine]:
-		$DialogBox/NinePatchRect/Dialog.text = DialogueBox.fillVariableDialogText(lines[currentLine]['text'])
-	elif determineIfClearLine():
-		$DialogBox/NinePatchRect/Dialog.text = ''
-	if 'name' in lines[currentLine]:
-		$DialogBox/NinePatchRect/Name.text = DialogueBox.fillVariableDialogText(lines[currentLine]['name'])
-	elif determineIfClearLine():
-		$DialogBox/NinePatchRect/Name.text = ''
-
-func updatePatienceDecay():
-	if 'patienceDecay' in lines[currentLine]:
-		$PatienceBar.setDecay(lines[currentLine]['patienceDecay'])
-
-func updateActions():
-	if 'actions' in lines[currentLine]:
-		var actions = lines[currentLine]['actions']
-		for i in range(len(actions)):
-			if 'actionText' in actions[i]:
-				get_node(buttons[i]).text = DialogueBox.fillVariableDialogText(actions[i]['actionText'])
-			if 'addPatience' in actions[i]:
-				get_node(buttons[i]).addPatience = actions[i]['addPatience']
-			if 'skipToLine' in actions[i]:
-				get_node(buttons[i]).skipToLine = actions[i]['skipToLine']
-			else:
-				get_node(buttons[i]).skipToLine = -1
-	elif determineIfClearLine():
-		for button in buttons:
-			get_node(button).skipToLine = -1
-			get_node(button).addPatience = 0
-			get_node(button).text = ''
-	else:
-		for button in buttons:
-			get_node(button).skipToLine = -1
-
-func updateSprite(): 
-	if 'character' in lines[currentLine]:
-		character = lines[currentLine]['character']
-	elif determineIfClearLine():
-		character = ''
-	if 'enemySprite' in lines[currentLine]:
-		 $EnemySprite.texture = load(defaultFilePath + character + '/' + lines[currentLine]['enemySprite'])
-	elif determineIfClearLine():
-		$EnemySprite.texture = load('')
+extends "res://assets/ui/DialogBox.gd"
 
 func battleWon():
 	print("you won ig");
@@ -98,24 +7,20 @@ func battleWon():
 func battleLost():
 	print("you lost");
 	hide()
- 
-func show():
-	self.visible = true
 
-func hide():
-	self.visible = false
+func endOfDialogReached():
+	battleWon()
 
-#func _input(event):
-#	if event.is_action_pressed("interact"):
-#		nextLine()
+func updatePatienceDecay():
+	if 'patienceDecay' in lines[currentLine]:
+		$PatienceBar.setDecay(lines[currentLine]['patienceDecay'])
 
-func loadBattle():
-	var file = File.new()
-	if file.file_exists(battleFile):
-		file.open(battleFile, file.READ)
-		return parse_json(file.get_as_text())
+#func updateButtonPatience():
+#	if 'addPatience' in actions[i]:
+#		get_node(buttons[i]).addPatience = actions[i]['addPatience']
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func updateUI():
+	updateDialog()
+	updatePatienceDecay()
+	updateActions()
+	updateSprite()
---
+++ assets/ui/DialogBox.gd
@@ -0,0 +1,135 @@
+extends Node2D
+
+const formatStrings = {}
+
+static func addFormatStrings(key, replacement) :
+	formatStrings[key] = replacement
+
+static func fillVariableDialogText(rawDialog) :
+	for key in formatStrings :
+		rawDialog = rawDialog.replace(key, formatStrings[key])
+		# replaces keywords like <name> with the actual player name
+	return rawDialog
+
+const defaultFilePath = "res://assets/characters/"
+
+export(String) var character
+# the name of the folder where the character sprites will be accessed from
+export(String, FILE, "*.json") var dialogFile = "res://assets/ui/testBattle.json"
+# the file where the dialog will be read from
+var lines = []
+# list lines, will be populated after parsing the json
+var currentLine = 0
+var buttons = ["NinePatchRect/Choice1", "NinePatchRect/Choice2"]
+# names of the button objects that will be used
+var forceChoice = false
+# variable that tells the game whether or not to force the player to press
+# the buttons or use advance text using the interact button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	addFormatStrings("<name>", "Bob")
+	play()
+	currentLine = -1
+	advanceLine()
+
+func play():
+	show()
+	lines = loadDialog()
+
+func advanceLine():
+	currentLine += 1
+	if !checkOutOfBounds() :
+		if 'skipToLine' in lines[currentLine - 1]:
+			skipToLine(lines[currentLine - 1]['skipToLine'])
+		updateUI()
+
+func skipToLine(line):
+	currentLine = line
+	checkOutOfBounds()
+	updateUI()
+
+func checkOutOfBounds():
+	if currentLine >= len(lines):
+		endOfDialogReached()
+		return true
+
+func endOfDialogReached():
+	hide()
+
+func updateUI():
+	updateDialog()
+	updateActions()
+	updateSprite()
+
+func determineIfClearLine():
+	return 'clear' in lines[currentLine] and lines[currentLine]['clear']
+
+func updateDialog():
+	if 'text' in lines[currentLine]:
+		$NinePatchRect/Dialog.text = fillVariableDialogText(lines[currentLine]['text'])
+	elif determineIfClearLine():
+		$NinePatchRect/Dialog.text = ''
+	if 'name' in lines[currentLine]:
+		$NinePatchRect/Name.text = fillVariableDialogText(lines[currentLine]['name'])
+	elif determineIfClearLine():
+		$NinePatchRect/Name.text = ''
+
+func updateActions():
+	if 'actions' in lines[currentLine]:
+		forceChoice = true
+		var actions = lines[currentLine]['actions']
+		for i in range(len(actions)):
+			get_node(buttons[i]).show()
+			if 'actionText' in actions[i]:
+				get_node(buttons[i]).text = fillVariableDialogText(actions[i]['actionText'])
+#			if 'addPatience' in actions[i]:
+#				get_node(buttons[i]).addPatience = actions[i]['addPatience']
+			if 'skipToLine' in actions[i]:
+				get_node(buttons[i]).skipToLine = actions[i]['skipToLine']
+				actions[i]['skipToLine']
+			else:
+				get_node(buttons[i]).skipToLine = -1
+	elif determineIfClearLine():
+		for button in buttons:
+			var buttonNode = get_node(button)
+			buttonNode.skipToLine = -1
+			buttonNode.addPatience = 0
+			buttonNode.text = ''
+	else:
+		forceChoice = false
+		for button in buttons:
+			get_node(button).skipToLine = -1
+			get_node(button).text = ""
+			get_node(button).hide()
+
+func updateSprite(): 
+	if 'character' in lines[currentLine]:
+		character = lines[currentLine]['character']
+	elif determineIfClearLine():
+		character = ''
+	if 'sprite' in lines[currentLine]:
+		$sprite.texture = load(defaultFilePath + character + '/' + lines[currentLine]['sprite'])
+	elif determineIfClearLine():
+		$sprite.texture = null
+
+func show():
+	self.visible = true
+
+func hide():
+	self.visible = false
+
+func _input(event):
+	if event.is_action_pressed("interact") and !forceChoice:
+		advanceLine()
+
+func loadDialog():
+	var file = File.new()
+	if file.file_exists(dialogFile):
+		file.open(dialogFile, file.READ)
+		return parse_json(file.get_as_text())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
