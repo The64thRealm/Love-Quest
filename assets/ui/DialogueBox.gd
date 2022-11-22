1dff1154ae3c7a37dd07cac3e488c1f6023c5d47
ikenton <imkenton@cpp.edu>
2022-11-22 15:31:34 -0800

    subway cutscene

--- assets/ui/DialogueBox.gd
+++ assets/ui/DialogueBox.gd
@@ -10,6 +10,9 @@ var lines = []
 var currentLine = 0
 var typing_speed = 0.1
 var read_time = 2
+var current_message = 0
+var display = ""
+var current_char = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,9 +23,9 @@ func _ready():
 
 func play():
 	show()
-	$NinePatchRect/Dialog.margin_right = $NinePatchRect.margin_right - 5
-	$NinePatchRect/Name.margin_right = $NinePatchRect.margin_right - 5
-	$NinePatchRect/Name.margin_bottom = $NinePatchRect.margin_bottom - 5
+	#$NinePatchRect/Dialog.margin_right = $NinePatchRect.margin_right - 5
+	#$NinePatchRect/Name.margin_right = $NinePatchRect.margin_right - 5
+	#$NinePatchRect/Name.margin_bottom = $NinePatchRect.margin_bottom - 5
 	lines = loadDialog()
 
 func nextLine():
@@ -45,6 +48,9 @@ func _input(event):
 		nextLine()
 
 func loadDialog():
+	current_message = 0
+	display = ""
+	current_char = 0
 	var file = File.new()
 	if file.file_exists(dialogFile):
 		file.open(dialogFile, file.READ)
--- themes/SubwayCut.gd
+++ themes/SubwayCut.gd
@@ -1,19 +1,13 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-func _musicStopped():
-	if(not playing):
-		get_tree().change_scene()
+	$Timer.start()
+	$subway.play()
 	
+	#if(test == false):
+	#	get_tree().change_scene("res://scenes/city2.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://scenes/city 2.tscn")
