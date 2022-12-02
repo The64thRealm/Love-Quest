7a5712843ad0dc839eedb1a16da44523179e740b
Natasha <wong.natasha.m@gmail.com>
2022-12-01 23:48:45 -0800

    apartment dialog + battle transition

--- scripts/GUI3.gd
+++ scripts/GUI3.gd
@@ -1,12 +1,14 @@
 extends CanvasLayer
 
-
 func _ready():
-	
-	player.cantMove()
-	player.idleDown()
+	$"../stuff/addons/player2".cantMove()
+	$"../stuff/addons/player2".idleUp()
 	var harper = preload("res://scenes/harper.tscn").instance()
 	harper.idleDown()
-	harper.position = Vector2(-31.0,-43.0)
+	harper.position = Vector2(-39.0,15.0)
 	get_tree().get_root().get_node("Node2D/base/stuff/addons/behind/TileMap").add_child(harper)
 	$DialogBox.play()
+
+
+func _on_DialogBox_endReached():
+	OfficeTransition.change_scene("res://assets/ui/BattleMinigames/DodgeGame/DodgeGame.tscn")
